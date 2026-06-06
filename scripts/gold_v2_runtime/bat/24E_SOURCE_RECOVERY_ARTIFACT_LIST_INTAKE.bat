9667ec60b050d7a13a0f07ee15e66836e041e12a
knitanr-a11y <k.nitan.r@gmail.com>
2026-06-06 21:28:10 +0900

    Point GOLD V2 24E BAT to hardened artifact list intake

--- scripts/gold_v2_runtime/bat/24E_SOURCE_RECOVERY_ARTIFACT_LIST_INTAKE.bat
+++ scripts/gold_v2_runtime/bat/24E_SOURCE_RECOVERY_ARTIFACT_LIST_INTAKE.bat
@@ -1,4 +1,4 @@
 @echo off
 cd /d "%~dp0\..\..\.."
-python scripts\gold_v2_runtime\audit_gold_v2_24e_source_recovery_artifact_list_intake.py
+python scripts\gold_v2_runtime\audit_gold_v2_24e_source_recovery_artifact_list_intake_hardened.py
 pause
