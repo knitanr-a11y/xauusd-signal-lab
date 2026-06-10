a8fcb5bbf554f2cf7d6048b1f90056f89e034b72
knitanr-a11y <k.nitan.r@gmail.com>
2026-06-11 01:59:29 +0900

    Add GOLD V3 Stage94 plan BAT

---
+++ scripts/gold_v3_runtime/bat/run_gold_v3_94_plan.bat
@@ -0,0 +1,19 @@
+@echo off
+setlocal EnableExtensions
+set "D=%~dp0"
+cd /d "%D%\..\..\.."
+set "R=%CD%"
+set "F="
+if exist "%R%\..\..\goldsharp_m15.csv" set "F=%R%\..\.."
+if "%F%"=="" if exist "%R%\..\goldsharp_m15.csv" set "F=%R%\.."
+if "%F%"=="" if exist "%R%\Files\goldsharp_m15.csv" set "F=%R%\Files"
+if "%F%"=="" if exist "%R%\goldsharp_m15.csv" set "F=%R%"
+if "%F%"=="" if exist "%R%\..\FX_OUTPUTS\gold_v3" set "F=%R%\.."
+if "%F%"=="" exit /b 1
+for %%I in ("%F%") do set "F=%%~fI"
+python "%R%\scripts\gold_v3_runtime\gold_v3_94_signal_gated_stage80_sidecar_patch_plan_audit.py" --candle-dir "%F%"
+set "E=%ERRORLEVEL%"
+echo.
+echo Paste: %F%\FX_OUTPUTS\gold_v3\94c\paste_me.txt
+pause
+exit /b %E%
