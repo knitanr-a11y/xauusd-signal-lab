71888af34a4d3e5892f9b00a8531990a8f7f989f
knitanr-a11y <k.nitan.r@gmail.com>
2026-06-14 10:35:22 +0900

    Add Stage107GY silent runner

---
+++ scripts/gold_v3_runtime/gold_v3_107gy_silent_runner.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+import warnings
+warnings.filterwarnings('ignore', category=FutureWarning)
+
+import gold_v3_107gy_light_non_calendar_subfilter_search_audit as stage
+
+if __name__ == '__main__':
+    raise SystemExit(stage.main())
