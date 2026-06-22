a557b7ce2561deabcc032a497f347aa3a6d5199a
knitanr-a11y <k.nitan.r@gmail.com>
2026-06-23 01:21:30 +0900

    Expose Stage289 frozen model loader through feature API

--- scripts/gold_v3_runtime/gold_v3_289_live_features.py
+++ scripts/gold_v3_runtime/gold_v3_289_live_features.py
@@ -5,3 +5,4 @@ from gold_v3_289_feature_core import *
 from gold_v3_289_stage280_features import build_stage280_context,stage280_model_frame
 from gold_v3_289_stage281_features import build_stage281_context,stage281_model_frame
 from gold_v3_289_trigger_features import add_external_short_features,m5_trigger_frame,find_trigger
+from gold_v3_289_artifacts import load_frozen_booster as load_booster
