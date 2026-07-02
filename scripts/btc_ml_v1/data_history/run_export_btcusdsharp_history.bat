796b825d1ee155cbd642b627c28dabe905c82d6c
knitanr-a11y <k.nitan.r@gmail.com>
2026-07-02 10:31:35 +0900

    Use fail-safe BTC history export runner

--- scripts/btc_ml_v1/data_history/run_export_btcusdsharp_history.bat
+++ scripts/btc_ml_v1/data_history/run_export_btcusdsharp_history.bat
@@ -1,5 +1,5 @@
 @echo off
 setlocal
 cd /d "%~dp0\..\..\..\"
-python scripts\btc_ml_v1\data_history\export_btcusdsharp_history.py %*
+python scripts\btc_ml_v1\data_history\run_btcusdsharp_history.py %*
 exit /b %ERRORLEVEL%
