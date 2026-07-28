5467c8cf67d47903f7269f4b507c35ace8792c01
knitanr-a11y <k.nitan.r@gmail.com>
2026-07-29 05:31:10 +0900

    Remove root BTC availability launcher after organized move

--- RUN_BTC_FRESH_FORWARD_AVAILABILITY_AUDIT.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal EnableExtensions DisableDelayedExpansion
-cd /d "%~dp0"
-python scripts\btc_ml_v1\research\audit_btc_fresh_forward_availability.py %*
-set "EXITCODE=%ERRORLEVEL%"
-echo.
-echo BTC fresh forward availability audit exit code: %EXITCODE%
-echo Output: outputs\btc_ml_v1\btc_fresh_forward_availability_audit
-exit /b %EXITCODE%
