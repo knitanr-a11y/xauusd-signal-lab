d9fe294249e9c7876eab2ae5519b05f6de352aae
knitanr-a11y <k.nitan.r@gmail.com>
2026-08-01 04:36:18 +0900

    Add Discord entry alert loop helper

---
+++ scripts/gold_wave_shadow_v19/08_RUN_DISCORD_ALERTS.bat
@@ -0,0 +1,12 @@
+@echo off
+setlocal
+for %%I in ("%~dp0..\..") do set "ROOT=%%~fI"
+set "CONFIG=%ROOT%\config\gold_wave_shadow_v19\local_config.json"
+set "PY=%LOCALAPPDATA%\xauusd_signal_lab\gold_v19_shadow\venv\Scripts\python.exe"
+if not exist "%PY%" exit /b 2
+if not exist "%CONFIG%" exit /b 2
+pushd "%ROOT%"
+"%PY%" -m scripts.gold_wave_shadow_v19.discord_notifier --config "%CONFIG%" loop
+set "RC=%ERRORLEVEL%"
+popd
+exit /b %RC%
