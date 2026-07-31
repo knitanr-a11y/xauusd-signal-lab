31f35b17ebac86ed29c384ade15563e4783c4533
knitanr-a11y <k.nitan.r@gmail.com>
2026-08-01 04:36:04 +0900

    Add Discord connection test helper

---
+++ scripts/gold_wave_shadow_v19/07_TEST_DISCORD.bat
@@ -0,0 +1,13 @@
+@echo off
+setlocal
+for %%I in ("%~dp0..\..") do set "ROOT=%%~fI"
+set "CONFIG=%ROOT%\config\gold_wave_shadow_v19\local_config.json"
+set "PY=%LOCALAPPDATA%\xauusd_signal_lab\gold_v19_shadow\venv\Scripts\python.exe"
+if not exist "%PY%" exit /b 2
+if not exist "%CONFIG%" exit /b 2
+pushd "%ROOT%"
+"%PY%" -m scripts.gold_wave_shadow_v19.discord_notifier --config "%CONFIG%" test
+set "RC=%ERRORLEVEL%"
+popd
+pause
+exit /b %RC%
